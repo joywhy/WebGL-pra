068e90d3d2e3673ae74b3df452e59b2818736756
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-09-13 00:18:45 +0900

    feat: 큐브 생성

--- cube/src/Cube.tsx
+++ cube/src/Cube.tsx
@@ -1,16 +1,35 @@
+import {Box as ChackraBok} from "@chakra-ui/react";
+import { Canvas,useFrame} from "@react-three/fiber";
+import { Mesh } from 'three';
+import { useRef } from "react";
 
-import {Canvas} from '@react-three/fiber';
 const Cube = () => {
+    const Box = (props:JSX.IntrinsicElements['mesh'])=>{
+         const ref = useRef<Mesh>(null);
+
+         useFrame(()=>{
+            ref.current.rotation.x += 0.01
+         })
+        return (
+            <mesh ref={ref} scale ={2} {...props}>
+                <boxGeometry arges={[1,1,1]}/>
+                <meshStandardMaterial color ="orange"/>
+            </mesh>
+        );
+    }
+
+
     return (
-        <div>
-        <Canvas>
-        {/* <Mesh position={[0,0,0]}>
-          <boxBufferGeometry attatch="geometry" /> 
-         
-          <meshLambertMaterial attatch="material" color="orange" />
-        </Mesh> */}
-         </Canvas>
-        </div>
+       <ChackraBok bg ="black">
+        <Canvas dpr = {[1,2]}>
+            <ambientLight intensity ={0.5}/>
+            <spotLight position={[10,10,10]} angle ={0.5} penumbra ={1}/>
+            <pointLight position={[-10,-10,-10]}/>
+            <Box position={[-3.2,0,0]}/>
+            <Box position={[4.2,0,0]}/>
+        </Canvas>
+       </ChackraBok>
+        
     );
 };
 
