43045fe36c51ce2c9e12ebdbfeb9005dcfd6fa90
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-09-12 22:25:16 +0900

    init: 리액트 three 관련 라이브러리 세팅

---
+++ cube/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Cube from './Cube'
+function App() {
+
+  return (
+    <>
+      hello
+      <Cube/>
+    </>
+  )
+}
+
+export default App
---
+++ cube/src/Cube.tsx
@@ -0,0 +1,17 @@
+
+import {Canvas} from '@react-three/fiber';
+const Cube = () => {
+    return (
+        <div>
+        <Canvas>
+        {/* <Mesh position={[0,0,0]}>
+          <boxBufferGeometry attatch="geometry" /> 
+         
+          <meshLambertMaterial attatch="material" color="orange" />
+        </Mesh> */}
+         </Canvas>
+        </div>
+    );
+};
+
+export default Cube;
