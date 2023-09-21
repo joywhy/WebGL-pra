867fc36a12591cb94eb3f2f42092942364d0386b
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-09-21 21:19:22 +0900

    feat: gui를 이용한 오브젝트 제어

--- praThree/src/client/client.ts
+++ praThree/src/client/client.ts
@@ -3,6 +3,9 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from "three/examples/jsm/libs/stats.module"
 import { GUI } from 'dat.gui'
 const scene = new THREE.Scene()
+//스크린에 x축 , y축, z 축 가상선 생성하기 
+//AxesHelper는 LineSegments를 확장한 객체임
+ scene.add(new THREE.AxesHelper(5)) //5단위길이의 가상선
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -39,15 +42,30 @@ const stats = new Stats() //프레임 렌더링 계산
 document.body.appendChild(stats.dom)
 const gui = new GUI()
 const cubeFolder= gui.addFolder("cube")
-cubeFolder.add(cube.rotation,"x",0, Math.PI *2)
-cubeFolder.add(cube.rotation,"y",0, Math.PI *2)
-cubeFolder.add(cube.rotation,"z",0, Math.PI *2)
-//두번째 인자는 문자열, 3번째 최소값 4번째 최댓값
-cubeFolder.open()
 
-const cameraFolder = gui.addFolder("camera")
-cameraFolder.add(camera.position,"z",0,20)
-//cameraFolder.open()//폴더가 열린채가 디폴트가 된다.
+const cubeRotationFolder = cubeFolder.addFolder("Rotation");
+cubeRotationFolder.add(cube.rotation,"x",0, Math.PI *2)
+cubeRotationFolder.add(cube.rotation,"y",0, Math.PI *2)
+cubeRotationFolder.add(cube.rotation,"z",0, Math.PI *2)
+cubeFolder.open()
+cubeRotationFolder.open()
+
+const cubePositionFolder = cubeFolder.addFolder("Position");
+cubePositionFolder.add(cube.position,"x",-10, 10)
+cubePositionFolder.add(cube.position,"y",-10, 10)
+cubePositionFolder.add(cube.position,"z",-10, 10)
+cubePositionFolder.open()
+
+const cubeScaleFolder = cubeFolder.addFolder("scale");
+cubeScaleFolder.add(cube.scale,"x",0,5)
+cubeScaleFolder.add(cube.scale,"y",0,5)
+cubeScaleFolder.add(cube.scale,"z",0,5)
+
+cubeScaleFolder.open()
+
+cubeFolder.add(cube, 'visible')
+
+//폴더가 열린채가 디폴트가 된다.
 //그전에 카메라 z위치로 설정해준값이 디폴트로 자리잡아 랜더된다. 
 // 우리는 그전에 2로 설정했기때문에  2로 랜더된다.
 function animate() {
